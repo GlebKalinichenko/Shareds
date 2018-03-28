59787c1cda8c02c1102f14326c1cf318b9226b31
GlebKalinichenko <gleb.kalinichenko@umbrella-web.com>
2018-03-28 14:53:06 +0300

    Init project

---
+++ MyShareds/share/src/main/java/com/kalinichenko/gleb/share/MySharedTestEntity.java
@@ -0,0 +1,17 @@
+package com.kalinichenko.gleb.share;
+
+public class MySharedTestEntity {
+    private String name;
+
+    public MySharedTestEntity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
